758cc46454b467a78338585f98ed6d608fa2b0dc
Paul Grimes <paul.grimes@byprime.com>
2013-07-11 16:31:53 -0400

    Added simple variance algorithm.

---
+++ VarianceAlgorithm/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarianceAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = {1,4,5,3,5,6,7,8,4};
+
+            Console.WriteLine(Variance.simple_variance(myArray));
+        }
+    }
+}
